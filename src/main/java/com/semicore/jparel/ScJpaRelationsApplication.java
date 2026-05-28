415b888b28e47238eee222513fbdf3f124f48f71
Sekhar Babu Patnam <babuthetrainer@gmail.com>
2026-05-28 10:24:16 +0530

    One to one relation demo in JPA

---
+++ src/main/java/com/semicore/jparel/ScJpaRelationsApplication.java
@@ -0,0 +1,13 @@
+package com.semicore.jparel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScJpaRelationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScJpaRelationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/semicore/jparel/ScJpaRelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicore.jparel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScJpaRelationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
